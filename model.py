94fb528e4508de881d051d302d836dda3e025adb
Kelvin <kelvinmulyawan2000@gmail.com>
2022-07-15 00:55:01 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,94 +1,22 @@
-import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
-import urllib.request
-import zipfile
-from tensorflow.keras import models, layers
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-data_url = "https://github.com/dicodingacademy/assets/releases/download/release-rps/rps.zip"
-urllib.request.urlretrieve(data_url, "rps.zip")
-local_file = "rps.zip"
-zip_ref = zipfile.ZipFile(local_file, "r")
-zip_ref.extractall("data/")
-zip_ref.close()
+xs = np.array([1, 2, 3, 4, 5, 6], dtype=float)
+ys = np.array([(2*num)+1 for num in xs], dtype=float)
 
-main_path = "data/rps/"
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(units=1, input_shape=[1])
+])
 
-# Data Augmentation
-train_data = ImageDataGenerator(
-    rescale=1.0 / 255, shear_range=0.2, zoom_range=0.2, validation_split=0.2
+model.compile(
+    optimizer='sgd',
+    loss='mean_squared_error'
 )
 
-# Data Flow
-train_gen = train_data.flow_from_directory(
-    main_path, target_size=(150, 150), class_mode="categorical", subset="training"
-)
+model.fit(xs, ys, epochs=10)
 
-test_gen = train_data.flow_from_directory(
-    main_path, target_size=(150, 150), class_mode="categorical", subset="validation"
-)
+tr_ev = model.evaluate(xs, ys, verbose=0)
 
-
-model = tf.keras.models.Sequential(
-    [
-        tf.keras.layers.Conv2D(
-            16, (3, 3), activation="relu", input_shape=(150, 150, 3)
-        ),
-        tf.keras.layers.MaxPool2D(2, 2),
-        tf.keras.layers.Conv2D(16, (3, 3), activation="relu"),
-        tf.keras.layers.MaxPool2D(2, 2),
-        tf.keras.layers.Conv2D(32, (3, 3), activation="relu"),
-        tf.keras.layers.MaxPool2D(2, 2),
-        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
-        tf.keras.layers.MaxPool2D(2, 2),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dropout(0.8),
-        tf.keras.layers.Dense(256, activation="relu"),
-        tf.keras.layers.Dense(units=3, activation="softmax"),
-    ]
-)
-
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-
-history = model.fit(
-    train_gen, validation_data=test_gen, epochs=2, verbose=1, callbacks=[checkpoint]
-)
-
-train_evaluation = model.evaluate(train_gen, verbose=0)
-validation_evaluation = model.evaluate(test_gen, verbose=0)
-
-# Output Section
 with open("results.txt", "w") as outfile:
-    lt, at = "loss : "+str(train_evaluation[0]), "accuracy : "+str(train_evaluation[1])
-    lv, av = "loss : "+str(validation_evaluation[0]), "accuracy : "+str(validation_evaluation[1])
-    outfile.write("Train Evaluation")
-    output.write(lt)
-    output.write(at)
-    outfile.write("Validation Evaluation")
-    output.write(lv)
-    output.write(av)
-
-loss, accuracy = history.history["loss"], history.history["accuracy"]
-val_loss, val_accuracy = history.history["val_loss"], history.history["val_accuracy"]
-epochs = range(len(history.history["loss"]))
-
-
-# Plot loss
-plt.style.use("seaborn-whitegrid")
-plt.plot(epochs, loss, label="training_loss")
-plt.plot(epochs, val_loss, label="val_loss")
-plt.title("Loss")
-plt.xlabel("Epochs")
-plt.legend()
-plt.savefig("loss.png")
-
-# Plot accuracy
-plt.figure()
-plt.plot(epochs, accuracy, label="training_accuracy")
-plt.plot(epochs, val_accuracy, label="val_accuracy")
-plt.title("Accuracy")
-plt.xlabel("Epochs")
-plt.legend()
-plt.savefig("accuracy.png")
-
-plt.close()
+    outfile.write(str(tr_ev)))
+    outfile.write(str(model.predict([12])))
